e5854ae0bf2c88f3274129e48c62a88dcb191131
Poli Naidu <poli.sigilapalli.intl@cylogic.com>
2023-06-12 20:36:09 +0530

    handle exit from room

--- server/index.ts
+++ server/index.ts
@@ -6,46 +6,67 @@ let io = new Server({
     }
 })
 const server = io.listen(port);
-interface Rooms {
-    [roomId: string]: string[];
+interface RoomSockets {
+    [socketId: string]: Socket;
 }
-interface SignalDataInterface {
-  room : string;
-  answer ?: any;
-  candidate ?: any;
-  offer ?: any
+
+interface MySocket extends Socket {
+    channel : string;
+}
+
+interface Rooms {
+    [roomId: string]: RoomSockets;
+}
+interface PeerSignalingData {
+  peerId: string;
+  answer?: RTCSessionDescriptionInit;
+  candidate?: object;
+  offer?: RTCSessionDescriptionInit
 }
 
 const rooms: Rooms = {};
+const sockets : RoomSockets = {};
 
-io.on('connection', (socket: Socket) => {
+
+io.on('connection', (socketObj: Socket) => {
+  const socket = socketObj as MySocket;
+  sockets[socket.id] = socket;
   console.log('User connected');
 
   socket.on('join-room', (roomId: string) => {
     console.log('join-room',roomId, socket.id);
 
-    if (rooms[roomId]) {
-      rooms[roomId].push(socket.id);
-      // If there are two clients in the room, notify both of them
-      if (rooms[roomId].length === 2) {
-        console.log('emit joined')
-        io.to(rooms[roomId][0]).emit('joined', false); // Non-Initiator
-        io.to(rooms[roomId][1]).emit('joined', true); // initiator
-      }
-    } else {
-      rooms[roomId] = [socket.id];
+    if (roomId in socket.rooms) {
+      console.log("[" + socket.id + "] ERROR: already joined in ", roomId);
+      return;
     }
+
+    if (!rooms.hasOwnProperty(roomId)) {
+      rooms[roomId] = {};
+    }
+
+    for (let socketId in rooms[roomId]) {
+      rooms[roomId][socketId].emit('joined', {isInitiator : false, peerId : socket.id});
+      socket.emit('joined', {isInitiator : true, peerId : socketId});
+    }
+
+    rooms[roomId][socket.id] = socket;
+    socket.channel = roomId;
 
     socket.join(roomId);
   });
 
-  socket.on('signal', ({room, ...rest} : SignalDataInterface) => {
-    console.log('signal', {room, socketId : socket.id}, Object.keys(rest));
-    socket.broadcast.to('room1').emit('signal', rest);
+  socket.on('signal', ({ peerId, ...rest} : PeerSignalingData) => {
+    console.log('signal', {socketId : socket.id}, Object.keys(rest));
+    sockets[peerId].emit('signal', {peerId : socket.id, ...rest});
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User disconnected', socket.rooms, rooms);
+    if (rooms && rooms?.[socket.channel] && socket.id in rooms?.[socket.channel]) {
+      delete rooms[socket.channel][socket.id];
+    }
+    delete sockets[socket.id];
     // Here you could also handle removing the user from the rooms object
     
   });
